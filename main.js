18b84143ee0b38017d0422e583a37eb49954a90c
Brian McCavour <brian.mccavour@sap.com>
2018-11-22 16:32:19 -0500

    second commit

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ const server = jsonServer.create();
 const router = jsonServer.router('data.json');
 const middlewares = jsonServer.defaults();
 const cors = require('cors');
+const PORT = 3000;
 
 server.use(middlewares);
 server.use(router);
 server.use(cors());
-server.listen(3000, () => {
-  console.log('JSON Server is running')
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
 
 
